425b546a51f2478f6c2b31d084442cae2bb3189e
Mohan Chandra <mohan.chandra@oracle.com>
2017-04-18 11:03:07 +0530

    Correcting spelling error and adding hyperlink to CSC image

---
+++ V1/4-18-2017/Onboarding email body change.sql
@@ -0,0 +1,68 @@
+set define off;
+
+UPDATE RULE_CONFIGURATION SET RULE_VALUE = '<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head><META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=us-ascii"><meta name=Generator content="Microsoft Word 14 (filtered medium)"><!--[if !mso]><style>v\:* {behavior:url(#default#VML);}
+o\:* {behavior:url(#default#VML);}
+w\:* {behavior:url(#default#VML);}
+.shape {behavior:url(#default#VML);}
+</style><![endif]--><style><!--
+/* Font Definitions */
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+@font-face
+	{font-family:Tahoma;
+	panose-1:2 11 6 4 3 5 4 4 2 4;}
+/* Style Definitions */
+p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin:0in;
+	margin-bottom:.0001pt;
+	font-size:11.0pt;
+	font-family:"Calibri","sans-serif";}
+a:link, span.MsoHyperlink
+	{mso-style-priority:99;
+	color:blue;
+	text-decoration:underline;}
+a:visited, span.MsoHyperlinkFollowed
+	{mso-style-priority:99;
+	color:purple;
+	text-decoration:underline;}
+p.MsoAcetate, li.MsoAcetate, div.MsoAcetate
+	{mso-style-priority:99;
+	mso-style-link:"Balloon Text Char";
+	margin:0in;
+	margin-bottom:.0001pt;
+	font-size:8.0pt;
+	font-family:"Tahoma","sans-serif";}
+span.BalloonTextChar
+	{mso-style-name:"Balloon Text Char";
+	mso-style-priority:99;
+	mso-style-link:"Balloon Text";
+	font-family:"Tahoma","sans-serif";}
+span.EmailStyle19
+	{mso-style-type:personal;
+	font-family:"Calibri","sans-serif";
+	color:windowtext;}
+span.EmailStyle20
+	{mso-style-type:personal;
+	font-family:"Calibri","sans-serif";
+	color:#1F497D;}
+span.EmailStyle21
+	{mso-style-type:personal-reply;
+	font-family:"Calibri","sans-serif";
+	color:#1F497D;}
+.MsoChpDefault
+	{mso-style-type:export-only;
+	font-size:10.0pt;}
+@page WordSection1
+	{size:8.5in 11.0in;
+	margin:1.0in 1.0in 1.0in 1.0in;}
+div.WordSection1
+	{page:WordSection1;}
+--></style><!--[if gte mso 9]><xml>
+<o:shapedefaults v:ext="edit" spidmax="1026" />
+</xml><![endif]--><!--[if gte mso 9]><xml>
+<o:shapelayout v:ext="edit">
+<o:idmap v:ext="edit" data="1" />
+</o:shapelayout></xml><![endif]--></head><body lang=EN-US link=blue vlink=purple><div class=WordSection1><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''>Hi <<FIRST_NAME>>,<o:p></o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Congratulations for having completed the on-boarding stage of your Oracle Cloud journey.<o:p></o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''>Please let me introduce the <b>C</b>ustomer <b>S</b>uccess <b>C</b>onsultants, whose role is to answer questions as they arise, and to provide you with ongoing advice and guidance as needed so you can be successful in adopting our Cloud solutions.<o:p></o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''>They look forward to getting in touch with you and helping you on your Cloud journey. If you have any questions, please feel free to ask.<o:p></o:p></span></p><p class=MsoNormal><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''><o:p>&nbsp;</o:p></span></p><p class=MsoNormal align=center style=''''text-align:center''''><a href="https://www.hexicloud.com"><img width=348 height=203 id="_abc" src="https://www.hexicloud.com/css/img/csc.png"/></a><span style=''''font-size:12.0pt;font-family:"Arial","sans-serif"''''><o:p></o:p></span></p><p class=MsoNormal><o:p>&nbsp;</o:p></p></div></body></html>' WHERE RULE_KEY = 'ON_BOARDING_COMPLETION_EMAIL_BODY';
+
+commit;
